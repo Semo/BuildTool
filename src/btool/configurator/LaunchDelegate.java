5cc8ecefd084e77e02463b4f433fca893eba240a
semo <semox78@gmail.com>
2012-09-14 16:52:32 +0200

    Added Delegates

---
+++ src/btool/configurator/LaunchDelegate.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package btool.configurator;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+/**
+ * @author sebastian.morkisch
+ * 
+ */
+public class LaunchDelegate implements ILaunchConfigurationDelegate {
+
+	/**
+	 * @see
+	 * org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.
+	 * eclipse.debug.core.ILaunchConfiguration, java.lang.String,
+	 * org.eclipse.debug.core.ILaunch,
+	 * org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/btool/configurator/LaunchTabGroup.java
@@ -0,0 +1,18 @@
+package btool.configurator;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+
+public class LaunchTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	public LaunchTabGroup() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
